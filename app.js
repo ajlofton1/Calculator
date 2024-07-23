3b14e0e8a9c9ec85a410204b0f8a1f7f7d11b40c
ajlofton1 <arthurlofton4@gmail.com>
2024-07-23 16:59:01 +0000

    add

--- app.js
+++ app.js
@@ -11,4 +11,45 @@ let decimalAdded=false
 
 const operators=['+', '-', 'x', '/']
 
-function handleKeyPress(e){}
+function handleKeyPress(e){
+    const key = e.target.dataset.key
+    const lastchar = operation[operation.length - 1]
+    
+    if(key === '='){
+        return
+    }
+
+    if(key === '.' && decimalAdded){
+        return
+    }
+
+    if(operators.indexOf(key) !== -1){
+        decimalAdded = false
+    }
+
+    if (operation.length === 0 && key === '-'){
+        operation += key
+        input.innerHTML = operation 
+        return
+    }
+
+    if (operation.length === 0 && operators.indexOf(key) !== -1){
+        innput.innerHTML = operation 
+        return
+    }
+
+    if (operators.indexOf(lastchar) !== -1 && operators.indexOf(key)!==-1){
+        
+    }
+    if (key) {
+        if (key === '.') decimalAdded = true
+        operation += key
+        input.innerHTML = operation
+        return
+    }
+
+}
+
+    keys.forEach(key =>{
+        key.addEventListener ('click , handlekey press')
+    })
